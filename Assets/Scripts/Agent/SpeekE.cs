f69324a1ce00e6282c1002e1bd2c44fe17918b79
Althierfson <42945474+Althierfson@users.noreply.github.com>
2019-11-22 18:20:34 -0300

    fist commit
    copia direta do repositorio antigo

---
+++ Assets/Scripts/Agent/SpeekE.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Crosstales.RTVoice;
+using Crosstales.RTVoice.Model;
+
+
+public class SpeekE : MonoBehaviour{
+    
+    public AudioSource som;            // Audio da Voz
+    private Animator anim;             // Controle da animação
+    public GameObject ballonImage;     // Imagem do balão
+    public Text textBallon;            // Texto no balão
+    private string animState;          // Guarda a animação atual
+    Frase estadoAtual = new Frase();   // Objeto estado
+    Arquivos ctrArq;                   // Objeto para acesso a L/E nos arquivos
+    public int[] tabuleiro;
+    public int dica1, dica2;
+    public bool agentInWork = false;
+
+    void Start(){
+        anim = GetComponent<Animator>();   // Pegando a Animator para controle das animações
+        ballonImage.SetActive(false);      // esconde a imagem do balão
+        som = GetComponent<AudioSource>();
+
+        ctrArq = new Arquivos("1");        // Criando novo arquivo com ID informado
+        print(ctrArq.filePath);
+    }
+
+    public void Speek(){
+        // Quando essa função é chamada o TextToSpeech falará a frase contidade em estadoAtual.msg
+
+        //Speaker.Speak(text.text, null, Speaker.VoiceForName("Microsoft Daniel"));
+        Speaker.Speak(estadoAtual.msg, som, Speaker.VoiceForName("Microsoft Daniel"));
+        ctrArq.registra(estadoAtual.msg, estadoAtual.codigo, estadoAtual.emocao);
+    }
+
+    void Update(){
+        
+        // Controle da animação
+        animCtr();  // Ativando balão de animação
+    }
+
+    public void setPassivo(){
+        anim.SetBool(animState, false);
+        anim.SetBool("Passivo_1", true);
+    }
+
+    public void startAnimation(){
+        anim.SetBool(animState, true);
+        anim.SetBool("Passivo_1", false);
+    }
+
+    private void animCtr(){
+        /*
+            Responsavel por ativar o balão com a menssagem do Agente.
+        */
+
+        if(som.isPlaying == true){
+            ballonImage.SetActive(true);
+            this.agentInWork = true;
+        }
+        if(som.isPlaying == false){
+            ballonImage.SetActive(false);
+            this.agentInWork = false;
+        }
+    }
+
+    private void setConf(){
+        /*
+            Essa função é responsavel por receber um objeto frase e setar a frase no balão, na voz do robo e definir a animação
+        */
+        
+        Debug.Log(estadoAtual.msg);
+        textBallon.text = estadoAtual.msg;
+        animState = estadoAtual.emocao;
+        
+    }
+
+    public void reacao(bool rec){
+        /*
+            Função receber um sentimento e escolhe aleatoriamente uma frase no banco de frases com o sentimento correspondente
+        */
+
+        if (rec == true){
+            estadoAtual = ctrArq.pickUpEmocao("Alegre");
+        }else{
+
+            int sentimento = (int)Random.Range(1, 3);
+
+            if(sentimento == 1){
+                estadoAtual = ctrArq.pickUpEmocao("Triste");
+            }else if(sentimento == 2){
+                estadoAtual = ctrArq.pickUpEmocao("Bravo");
+            }else if(sentimento == 3){
+                estadoAtual = ctrArq.pickUpEmocao("Vergonha");
+            }else{
+                estadoAtual = ctrArq.pickUpEmocao("Triste");
+            }
+        }
+        if(estadoAtual != null){
+            setConf();
+
+            startAnimation();
+        }
+    }
+
+    public void gerarDicaCorreta(){
+        int dica1, dica2;
+
+        for(int i=0;i<tabuleiro.Length;i++){
+            dica1 = tabuleiro[i];
+
+            if(dica1 != -1){
+                for(int j=i+1;j<tabuleiro.Length;j++){
+                    dica2 = tabuleiro[j];
+
+                    if(dica1 == dica2){
+                        this.dica1 = i+1;
+                        this.dica2 = j+1;
+                        falarDica();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void gerarDicaFalsa(){
+        int escolha1 = (int)Random.Range(1, tabuleiro.Length);
+        int escolha2 = escolha1;
+        while(this.tabuleiro[escolha1] == this.tabuleiro[escolha2] || this.tabuleiro[escolha1] == -1 || this.tabuleiro[escolha2] == -1){
+            escolha2 = (int)Random.Range(1, tabuleiro.Length);
+        }
+
+        dica1 = escolha1;
+        dica2 = escolha2;
+        falarDica();
+    }
+
+    private void falarDica(){
+
+        estadoAtual = ctrArq.getFrases("2");
+        Debug.Log("arquivo speek: "+estadoAtual.msg);
+        estadoAtual.msg = "Esperimente virar a carta";
+
+        estadoAtual.msg = estadoAtual.msg+ " " +dica1.ToString()+ " e a "+ dica2.ToString()+ ".";
+        Debug.Log(estadoAtual.msg);
+
+        setConf();
+        Speek();
+        //startAnimation();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite bgImage;
+
+    public Sprite[] puzzles;
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
+    public List<Button> btns = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+    //private int[] tabuleiro;
+    private bool openToClick = true;
+
+    [SerializeField]
+    private SpeekE speekE;
+
+    private void Awake()
+    {
+        puzzles = Resources.LoadAll<Sprite>("Sprites/Candy");
+    }
+
+
+    private void Start()
+    {
+        GetButtons();
+        AddListeneers();
+        AddGamePuzzles();
+        Shuffle(gamePuzzles);
+        gameGuesses = gamePuzzles.Count / 2;
+        speekE.gerarDicaFalsa();
+    }
+
+        void GetButtons()
+    {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                btns.Add(objects[i].GetComponent<Button>());
+                btns[i].image.sprite = bgImage;
+            }
+    }
+
+    void AddGamePuzzles()
+    {
+        /*
+            Essa é a função responsavel por gerar o tabuleiros com os pares
+        */
+        int looper = btns.Count;
+        int index = 0;
+
+        speekE.tabuleiro = new int[looper];
+
+        for (int i=0; i< looper; i++)
+        {
+            if (index == looper /2)
+            {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+            speekE.tabuleiro[i] = index;
+
+            index++;
+        }
+    }
+
+    void AddListeneers()
+    {
+        foreach (Button btn in btns)
+        {
+            btn.onClick.AddListener(() => PickAPuzzle());
+        }
+    }
+
+    public void PickAPuzzle()
+    {
+        if(!speekE.agentInWork){
+            if (!firstGuess)
+            {
+                firstGuess = true;
+                firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+                firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+
+                btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+
+            } else if (!secondGuess)
+            {
+                secondGuess = true;
+                secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+                secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+
+                btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+
+                if(firstGuessPuzzle == secondGuessPuzzle){
+                    Debug.Log("Cartas corretas");
+                    trataInteracao();
+                } else {
+                    Debug.Log("Tente outra vez");
+                    trataInteracao();
+                }
+
+                countGuesses++;
+
+                StartCoroutine(CheckIfThePuzzlesMatch());
+
+            }
+        }
+    }
+    IEnumerator CheckIfThePuzzlesMatch()
+    {
+        yield return new WaitForSeconds(1f);
+        if (firstGuessPuzzle == secondGuessPuzzle)
+        {
+            yield return new WaitForSeconds(.5f);
+
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+
+            btns[firstGuessIndex].image.color = new Color(1, 1, 1, 1);
+            btns[secondGuessIndex].image.color = new Color(1, 1, 1, 1);
+
+            CheckIfTheGameIsFinished();
+
+        } else
+        {
+            yield return new WaitForSeconds(.5f);
+
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+        }
+
+        yield return new WaitForSeconds(.5f);
+
+        firstGuess = secondGuess = false;
+
+    }
+
+    void CheckIfTheGameIsFinished()
+    {
+        countCorrectGuesses++;
+
+        if (countCorrectGuesses == gameGuesses)
+        {
+            Debug.Log("Fim do Jogo");
+            Debug.Log("Foram " + countGuesses + " tentativas para vencer");
+
+        }
+    }
+
+    void Shuffle(List<Sprite> list) // funcao randomica
+    {
+        for (int i=0; i < list.Count; i++)
+       {
+            Sprite temp = list[i];
+            int inttemp = speekE.tabuleiro[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            speekE.tabuleiro[i] = speekE.tabuleiro[randomIndex];
+            list[randomIndex] = temp;
+            speekE.tabuleiro[randomIndex] = inttemp;
+        }
+    }
+
+    void trataInteracao(){
+        // Função responcavel por trata o acerto de cartas
+
+        speekE.tabuleiro[firstGuessIndex] = -1;
+        speekE.tabuleiro[secondGuessIndex] = -1;
+
+        if(firstGuessIndex+1 == speekE.dica1 || firstGuessIndex+1 == speekE.dica2){
+            if(secondGuessIndex+1 == speekE.dica1 || secondGuessIndex+1 == speekE.dica2){
+                blockInteracao();
+                speekE.reacao(true);
+                openInteracao();
+                Debug.Log("GameController: Usuario escolheo as cartas do Agente");
+            }else{
+                blockInteracao();
+                speekE.reacao(false);
+                openInteracao();
+                Debug.Log("GameController: Usuario não escolheo as cartas do Agente");
+            }
+        }else{
+            speekE.reacao(false);
+            Debug.Log("GameController: Usuario não escolheo as cartas do Agente");
+        }
+    }
+
+    public void blockInteracao(){
+        openToClick = false;
+    }
+
+    public void openInteracao(){
+        openToClick = true;
+    }
+
+} // GameController
